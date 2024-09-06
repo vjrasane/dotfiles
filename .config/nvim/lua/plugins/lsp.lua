c25844bcf401cc4cc34f1ab93039b5b7329e7cfc
Ville Räsänen <vjrasane@gmail.com>
2024-09-06 20:27:59 +0000

    add docker install

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -22,7 +22,7 @@ local servers = {
 	--
 
 	astro = {},
-	yamlls = {},
+	-- yamlls = {},
 	lua_ls = {
 		-- cmd = {...},
 		-- filetypes = { ...},
@@ -159,7 +159,7 @@ return {
 
 			-- Useful status updates for LSP.
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", opts = {} },
+			{ "j-hui/fidget.nvim",       opts = {} },
 
 			-- Allows extra capabilities provided by nvim-cmp
 			"hrsh7th/cmp-nvim-lsp",
