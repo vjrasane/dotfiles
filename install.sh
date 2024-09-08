8599429777dce3d59297e9112b74c6c193463d6f
Ville Räsänen <vjrasane@gmail.com>
2024-09-08 10:27:00 +0000

    more install

--- install.sh
+++ install.sh
@@ -5,4 +5,15 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
-ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml --ask-become-pass
+sudo apt install -y curl
+
+get_file_from_repo() {
+  local path="$1"
+  local output="$2"
+  curl -fsSL https://github.com/vjrasane/dotfiles/raw/main/"$path" >"$output"
+}
+
+playbook=/tmp/playbook.yml
+get_file_from_repo playbook.yml "$playbook"
+
+ansible-playbook --connection=local --inventory 127.0.0.1, "$playbook" --ask-become-pass
