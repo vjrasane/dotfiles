01a39be6a8cbad05ad485e2fad39aba13981e33f
Ville Räsänen <vjrasane@gmail.com>
2024-09-29 23:36:13 +0300

    add some output to install

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "#######################################################"
+echo "# Installing requirements"
+echo "#######################################################"
+
 sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
@@ -13,7 +17,15 @@ get_file_from_repo() {
   curl -fsSL "$repo"/raw/main/"$path" >"$output"
 }
 
+echo "#######################################################"
+echo "# Fetching files"
+echo "#######################################################"
+
 playbook=/tmp/playbook.yml
 get_file_from_repo playbook.yml "$playbook"
+
+echo "#######################################################"
+echo "# Running Ansible"
+echo "#######################################################"
 
 ansible-playbook --connection=local --inventory 127.0.0.1, "$playbook" --ask-become-pass
