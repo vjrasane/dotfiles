7bae9687b5643561e73bcd79e2130b8a2421a80e
Ville Räsänen <vjrasane@gmail.com>
2024-09-30 19:39:48 +0300

    trying to make python lsp work

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,44 +1,12 @@
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. Available keys are:
---  - cmd (table): Override the default command used to start the server
---  - filetypes (table): Override the default list of associated filetypes for the server
---  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
---  - settings (table): Override the default settings passed when initializing the server.
---        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-local servers = {
-	-- clangd = {},
-	-- gopls = {},
-	-- pyright = {},
-	-- rust_analyzer = {},
-	-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-	--
-	-- Some languages (like typescript) have entire language plugins that can be useful:
-	--    https://github.com/pmizio/typescript-tools.nvim
-	--
-	-- But for many setups, the LSP (`tsserver`) will work just fine
-	ts_ls = {},
-	--
-	pyright = {},
-
-	astro = {},
-	-- yamlls = {},
-	lua_ls = {
-		-- cmd = {...},
-		-- filetypes = { ...},
-		-- capabilities = {},
-		settings = {
-			Lua = {
-				completion = {
-					callSnippet = "Replace",
-				},
-				-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-				-- diagnostics = { disable = { 'missing-fields' } },
-			},
-		},
-	},
-}
+local make_client_capabilities = function()
+	-- LSP servers and clients are able to communicate to each other what features they support.
+	--  By default, Neovim doesn't support everything that is in the LSP specification.
+	--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+	--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+	return capabilities
+end
 
 --  This function gets run when an LSP attaches to a particular buffer.
 --    That is to say, every time a new file is opened that is associated with
@@ -133,7 +101,6 @@ local on_attach = function(event)
 		end, "[T]oggle Inlay [H]ints")
 	end
 end
-
 return {
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -170,12 +137,6 @@ return {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = on_attach,
 			})
-			-- LSP servers and clients are able to communicate to each other what features they support.
-			--  By default, Neovim doesn't support everything that is in the LSP specification.
-			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 			-- Ensure the servers and tools above are installed
 			--  To check the current status of installed tools and/or manually install
@@ -184,7 +145,76 @@ return {
 			--
 			--  You can press `g?` for help in this menu.
 			require("mason").setup()
+			-- local pylsp = require("mason-registry").get_package("python-lsp-server")
+			-- pylsp:on("install:success", function()
+			-- 	local function mason_package_path(package)
+			-- 		local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
+			-- 		return path
+			-- 	end
+			--
+			-- 	local path = mason_package_path("python-lsp-server")
+			-- 	local command = path .. "/venv/bin/pip"
+			-- 	local args = {
+			-- 		"install",
+			-- 		"-U",
+			-- 		"pylsp-rope",
+			-- "python-lsp-black",
+			-- "pyflakes",
+			-- "python-lsp-ruff",
+			-- "pyls-flake8",
+			-- "sqlalchemy-stubs",
+			-- }
 
+			-- 	require("plenary.job")
+			-- 		:new({
+			-- 			command = command,
+			-- 			args = args,
+			-- 			cwd = path,
+			-- 		})
+			-- 		:start()
+			-- end)
+
+			--  Add any additional override configuration in the following tables. Available keys are:
+			--  - cmd (table): Override the default command used to start the server
+			--  - filetypes (table): Override the default list of associated filetypes for the server
+			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+			--  - settings (table): Override the default settings passed when initializing the server.
+			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+			local servers = {
+				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+				--
+				-- Some languages (like typescript) have entire language plugins that can be useful:
+				--    https://github.com/pmizio/typescript-tools.nvim
+				--
+				-- But for many setups, the LSP (`tsserver`) will work just fine
+				ts_ls = {},
+				ruff_lsp = {},
+				astro = {},
+				pyright = {
+					-- settings = {
+					-- 	python = {
+					-- 		analysis = {
+					-- 			autoImportCompletion = true,
+					-- 			-- autoSearchPaths = true,
+					-- 			-- diagnosticMode = "openFilesOnly",
+					-- 			useLibraryCodeForTypes = true,
+					-- 			-- typeCheckingMode = "off",
+					-- 		},
+					-- 	},
+					-- },
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+							-- diagnostics = { disable = { 'missing-fields' } },
+						},
+					},
+				},
+			}
 			-- You can add other tools here that you want Mason to install
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
@@ -193,6 +223,7 @@ return {
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
+			local capabilities = make_client_capabilities()
 			require("mason-lspconfig").setup({
 				handlers = {
 					function(server_name)
@@ -219,27 +250,14 @@ return {
 	},
 	{
 		"HallerPatrick/py_lsp.nvim",
-		-- event = "LspAttach",
-		dependencies = { "dharmx/toml.nvim" },
 		ft = { "python" },
-		opts = {
-			language_server = "pyright",
-			source_strategies = { "poetry", "default", "system" },
-			pylsp_plugins = {
-				autopep8 = {
-					enabled = true,
-				},
-				pyls_mypy = {
-					enabled = true,
-				},
-				pyls_isort = {
-					enabled = true,
-				},
-				flake8 = {
-					enabled = true,
-					executable = ".venv/bin/flake8",
-				},
-			},
-		},
+		config = function()
+			local capabilities = make_client_capabilities()
+			require("py_lsp").setup({
+				language_server = "pyright",
+				source_strategies = { "poetry", "default", "system" },
+				capabilities = capabilities,
+			})
+		end,
 	},
 }
