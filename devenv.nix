bd0ad67982812950b0afd2a15556ed5fcc89435c
vjrasane <3148501+vjrasane@users.noreply.github.com>
2026-01-10 19:56:56 +0200

    precommit

--- devenv.nix
+++ devenv.nix
@@ -3,19 +3,16 @@
   ...
 }:
 {
-  pre-commit = {
-    default_stages = [ "pre-push" ];
-    hooks = {
-      nixfmt-rfc-style.enable = true;
-      check-shebang-scripts-are-executable.enable = true;
-      check-symlinks.enable = true;
-      check-yaml.enable = true;
-      ripsecrets.enable = true;
-      shellcheck.enable = true;
-      shfmt.enable = true;
-      typos.enable = true;
-      trim-trailing-whitespace.enable = true;
-      end-of-file-fixer.enable = true;
-    };
+  pre-commit.hooks = {
+    nixfmt-rfc-style.enable = true;
+    check-shebang-scripts-are-executable.enable = true;
+    check-symlinks.enable = true;
+    check-yaml.enable = true;
+    ripsecrets.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    typos.enable = true;
+    trim-trailing-whitespace.enable = true;
+    end-of-file-fixer.enable = true;
   };
 }
