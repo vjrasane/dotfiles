18b719da0ad82503b40b7d0ef3e7a0382087b55c
vjrasane <3148501+vjrasane@users.noreply.github.com>
2026-06-19 15:26:41 +0300

    oci ssh config

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -4,8 +4,13 @@
     enable = true;
     enableDefaultConfig = false;
     matchBlocks = {
-      ocivm = {
+      ocivm-a1 = {
         hostname = "152.70.169.143";
+        port = 2222;
+        user = "ubuntu";
+      };
+      ocivm-e2 = {
+        hostname = "89.168.112.189";
         port = 2222;
         user = "ubuntu";
       };
